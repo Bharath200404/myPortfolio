f3b77e7f76109b8de42771167eeea1eb74d65de7
bharath <bharathnayanabharathhs@gmail.com>
2026-05-29 20:54:29 +0530

    changed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -118,7 +118,7 @@ const containerVariants = {
             </Link>
 
             <a
-              href="https://drive.google.com/file/d/1KAO8eFGcoHf0pAGpMbK3kEzzy3XxC3vl/view?usp=sharing"
+              href="https://drive.google.com/file/d/1tH3Fd9g6OnvbXGLiR8QbJn9O-evcToSS/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               className="btn-secondary inline-flex items-center justify-center gap-2"
